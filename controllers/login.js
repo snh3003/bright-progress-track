5c804459095fa0bee644c10b29a5aa27d5d3be3a
snh3003 <shahsama542@gmail.com>
2021-08-05 19:53:30 +0530

    .

--- controllers/login.js
+++ controllers/login.js
@@ -2,18 +2,27 @@ const login = async (collection, email, password) => {
 
   const filter = {
     'email': email,
-    'password': password
+    // 'password': password
+    
   };
 
   let result = await collection
-    .findOne(filter, { projection: { '_id': 0 } })
+    .findOne(filter)
     // .toArray()
     .then(
       arrayData => {
-        if (filter.email === arrayData.email && filter.password === arrayData.password) {
+        if (filter.email === arrayData.email && password === arrayData.password) {
+          console.log(arrayData);
           return {
             'success': true
           }
+        }else if(filter.email === arrayData.email && arrayData.password === ""){
+          console.log(arrayData);
+          return {
+            'success': false,
+            "setPassword": true
+          }
+          
         }
         return {
           'success': false
