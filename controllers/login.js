46914608547fe7c3d3d7bb472f66b01edf6298b5
Rahul Gupta <42062368+rahul7668gupta@users.noreply.github.com>
2021-08-12 14:54:06 +0530

    npm start ADDED

--- controllers/login.js
+++ controllers/login.js
@@ -1,66 +1,5 @@
 var axios = require('axios');
 
-const login = async (collection, email, password) => {
-  const filter = {
-    'email': email,
-    // 'password': password
-  };
-
-  let thinkficResponse = await thinkficApiCall(filter.email)
-  // check if email exists in thinkfic
-  if (thinkficResponse.exists === true) {
-    // query email in db
-    var inDB = await dbQuery(collection, filter)
-    // check if email exists in db
-    if (inDB.exists) {
-      // check if password in empty string
-      if (inDB.password === '') {
-        // update with given password
-        let update = await collection.findOneAndUpdate(filter, { $set: { password } })
-        // say that user exists in db
-        return {
-          existsInDB: true,
-          setPassword: false
-        }
-      }
-      // if password was not an empty string, return this
-      return {
-        existsInDB: true,
-        success: true
-      }
-    } else {
-      // if email does not exist in db
-      // insert a new email user with empty password
-      await collection.insert({ email, password: '' })
-      // say that user exists and ask to set password
-      return {
-        existsInDB: true,
-        setPassword: true
-      }
-    }
-  }
-  // in case email does not exist in thinkfic
-  return {
-    exists: false,
-    inThinkfic: false
-  }
-}
-
-const dbQuery = async (collection, filter) => {
-  return await collection
-    .findOne(
-      filter,
-      { projection: { '_id': 0, email: 1, password: 1 } }
-    ).then(a => {
-      a.exists = true
-      return a
-    })
-    .catch(err => {
-      return {
-        exists: false
-      }
-    })
-}
 
 const thinkficApiCall = async (email) => {
 
@@ -97,6 +36,74 @@ const thinkficApiCall = async (email) => {
       }
     });
   return resp
+}
+
+const dbQuery = async (collection, filter) => {
+  return await collection
+    .findOne(
+      filter,
+      { projection: { '_id': 0, email: 1, password: 1 } }
+    ).then(a => {
+      a.exists = true
+      return a
+    })
+    .catch(err => {
+      return {
+        exists: false
+      }
+    })
+}
+
+const login = async (collection, email, password) => {
+  const filter = {
+    'email': email,
+    // 'password': password
+  };
+
+  let thinkficResponse = await thinkficApiCall(filter.email)
+  console.log(thinkficResponse);
+  // check if email exists in thinkfic
+  if (thinkficResponse.exists === true) {
+    // query email in db
+    var inDB = await dbQuery(collection, filter)
+    // check if email exists in db
+    if (inDB.exists) {
+      // check if password in empty string
+      if (inDB.password === '') {
+        console.log('1', inDB.password, password);
+        // update with given password
+        let update = await collection.findOneAndUpdate(filter, { $set: { password } })
+        // say that user exists in db
+        return {
+          exists: true,
+          setPassword: false
+        }
+      } else if (inDB.password === password) {
+        console.log('2', inDB.password, password);
+        // if password was same
+        return {
+          exists: true,
+          success: true
+        }
+      } else {
+        console.log('3', inDB.password, password);
+        // if email does not exist in db
+        // insert a new email user with empty password
+        await collection.insertOne({ email, password: '' })
+        // say that user exists and ask to set password
+        return {
+          exists: true,
+          setPassword: true
+        }
+      }
+    }
+  }
+  // console.log('4', inDB.password, password);
+  // in case email does not exist in thinkfic
+  return {
+    exists: false,
+    inThinkfic: false
+  }
 }
 
 module.exports = login;
